7efb37965076e9a8ae2fdf6f9f98d262c0999267
Mack <amack@proforma.com>
2018-03-26 14:22:29 -0400

    addNumber.push to team score

--- public/main.js
+++ public/main.js
@@ -1,14 +1,39 @@
+const getRandomNumber = () => {
+  return Math.ceil(Math.random() * 10);
+}
+
 angular
   .module("scoreboardApp", [])
   .controller("mainController", ($scope) => {
 
-    $scope.updateScore = "";
-    $scope.displayScore = () => {
-      $scope.updateName = $scope.score;
+    $scope.updateTeam1Score = "";
+    $scope.displayTeam1Score = () => {
+      $scope.updateTeam1Score = $scope.score;
     }
-    $scope.displayName = "";
-    $scope.updateName = () => {
-      $scope.displayName = $scope.name;
+
+    $scope.updateTeam2Score = "";
+    $scope.displayTeam2Score = () => {
+      $scope.updateTeam1Score = $scope.score;
+    }
+    $scope.displayTeam1Name = "";
+    $scope.updateTeam1Name = () => {
+      // $scope.displayTeam1Name = $scope.name;
+      $scope.displayTeam1Name = $scope.updateTeam1Name;
+    }
+
+    $scope.displayTeam2Name = "";
+    $scope.updateTeam2Name = () => {
+      // $scope.displayTeam1Name = $scope.name;
+      $scope.displayTeam2Name = $scope.updateTeam2Name;
+    }
+    $scope.updatTeam1Score = 0;
+    $scope.addNumber = () => {
+      $scope.addNumber.push(updateTeam1Score);
+    }
+
+    $scope.updatTeam2Score = 0;
+    $scope.addNumber = () => {
+      $scope.addNumber.push(updateTeam2Score);
     }
   }
   );
